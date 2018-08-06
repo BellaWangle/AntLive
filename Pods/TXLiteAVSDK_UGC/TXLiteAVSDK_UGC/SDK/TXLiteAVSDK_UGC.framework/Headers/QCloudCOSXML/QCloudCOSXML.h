12508a3eafff78b8487f1327e421116d6285f24d
毅力起 <626055886@qq.com>
2018-08-06 16:36:39 +0800

    一些提交

---
+++ Pods/TXLiteAVSDK_UGC/TXLiteAVSDK_UGC/SDK/TXLiteAVSDK_UGC.framework/Headers/QCloudCOSXML/QCloudCOSXML.h
@@ -0,0 +1,65 @@
+//
+//  QCloudCOSXML.h
+//  Pods
+//
+//  Created by Dong Zhao on 2017/5/2.
+//
+//
+
+#ifndef QCloudCOSXML_h
+#define QCloudCOSXML_h
+
+#import "QCloudCOSXMLService.h"
+#import "QCloudCOSXMLService+Transfer.h"
+#import "QCloudCOSXMLService+Manager.h"
+#import "QCloudGetObjectACLRequest.h"
+#import "QCloudPutObjectRequest.h"
+#import "QCloudInitiateMultipartUploadRequest.h"
+#import "QCloudCOSTransferMangerService.h"
+#import "QCloudCOSXMLUploadObjectRequest.h"
+#import "QCloudUploadObjectResult.h"
+#import "QCloudPutObjectACLRequest.h"
+#import "QCloudDeleteObjectRequest.h"
+#import "QCloudDeleteMultipleObjectRequest.h"
+#import "QCloudListMultipartRequest.h"
+#import "QCloudDeleteObjectInfo.h"
+#import "QCloudDeleteInfo.h"
+#import "QCloudHeadObjectRequest.h"
+#import "QCloudAppendObjectRequest.h"
+#import "QCloudGetObjectRequest.h"
+#import "QCloudGetObjectRequest+Custom.h"
+#import "QCloudCopyObjectResult.h"
+#import "QCloudPutObjectCopyRequest.h"
+#import "QCloudGetBucketRequest.h"
+#import "QCloudGetBucketACLRequest.h"
+#import "QCloudGetBucketCORSRequest.h"
+#import "QCloudGetBucketLocationRequest.h"
+#import "QCloudPutBucketACLRequest.h"
+#import "QCloudPutBucketCORSRequest.h"
+#import "QCloudDeleteBucketCORSRequest.h"
+#import "QCloudListBucketMultipartUploadsRequest.h"
+#import "QCloudOptionsObjectRequest.h"
+#import "QCloudHeadBucketRequest.h"
+#import "QCloudCOSXMLEndPoint.h"
+#import "QCloudPutBucketRequest.h"
+#import "QCloudDeleteBucketRequest.h"
+#import "QCloudLifecycleConfiguration.h"
+#import "QCloudLifecycleRule.h"
+#import "QCloudPutBucketLifecycleRequest.h"
+#import "QCloudGetBucketLifecycleRequest.h"
+#import "QCloudDeleteBucketLifeCycleRequest.h"
+#import "QCloudPutBucketVersioningRequest.h"
+#import "QCloudGetBucketVersioningRequest.h"
+#import "QCloudBucketReplicationConfiguation.h"
+#import "QCloudPutBucketReplicationRequest.h"
+#import "QCloudGetBucketReplicationRequest.h"
+#import "QCloudDeleteBucketReplicationRequest.h"
+#import "QCloudGetServiceRequest.h"
+#import "QCloudUploadPartCopyRequest.h"
+#import "QCloudCOSXMLCopyObjectRequest.h"
+#import "QCloudPostObjectRestoreRequest.h"
+#import "QCloudRestoreRequest.h"
+#import "QCloudListObjectVersionsRequest.h"
+#import "QCloudGetPresignedURLRequest.h"
+#endif /* QCloudCOSXML_h */
+
